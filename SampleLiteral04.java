5f3703e838c24dbd1a9ec9748dcd22128a5d3416
xiaojinglin <takafumi7059@gmail.com>
2026-04-16 11:22:08 +0900

    test

---
+++ SampleLiteral04.java
@@ -0,0 +1,7 @@
+class SampleLiteral04 {
+    public static void main(String[] args) {
+        System.out.println('a');
+        System.out.println('\"');
+        System.out.println('\u0041');
+    }
+}
---
+++ SampleOp01.java
@@ -0,0 +1,9 @@
+class SampleOp01 {
+    public static void main(String[] args) {
+        System.out.println(12 + 3);
+        System.out.println(12 - 14);
+        System.out.println(12 * 4);
+        System.out.println(12 / 4);
+        System.out.println(12 % 10);
+    }
+}
