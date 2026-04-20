097b8de64520c2472b617028be98354ec454461e
xiaojinglin <takafumi7059@gmail.com>
2026-04-20 10:21:20 +0900

    java-learning

--- Test.java
+++ Test.java
@@ -1,5 +1,8 @@
 class Test{
     public static void main(String[] args){
-        System.out.println(1 + 2  * 4);
+        int x1 = 0L;
+        int x2 = 0;
+        System.out.println(x1 + ":" + x2);
+        
     }
 }
