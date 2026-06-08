f82ae1f75801f247ca0ca8c0decbd55108cb0f86
xiao-jinglin <takafumi7059@gmail.com>
2026-06-08 09:46:26 +0900

    java-learming

---
+++ SampleMethod02.java
@@ -0,0 +1,9 @@
+class SampleMethod02 {
+    public static void main(int x){
+        System.out.println("‚±‚ñ‚É‚¿‚ÍB");
+    }
+
+    public static void main(String[] args) {
+        main(5);
+    }
+}
